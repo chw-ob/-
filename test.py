07399b750778c872ae62b1c9599ad5e3c1d3c266
chw-ob <2252466462@qq.com>
2024-05-14 19:52:40 +0800

    加入状态变量分析

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
-import tkinter as tk
-
-def get_entry_value():
-    value = entry.get()
-    result_label.config(text="输入的值是：" + value)
-
-root = tk.Tk()
-root.title("获取Entry控件输入的值")
-
-entry = tk.Entry(root)
-entry.pack(padx=10, pady=10)
-
-get_value_button = tk.Button(root, text="获取值", command=get_entry_value)
-get_value_button.pack()
-
-result_label = tk.Label(root, text="")
-result_label.pack()
-
-root.mainloop()
+import numpy as np
+import matplotlib.pyplot as plt
+x=range(180)
+y=[]
+z=[]
+kopt=(0.33/0.07)**0.5
+for i in x:
+    g=np.sin((i+1)/2/180*np.pi)
+    g=g*40*kopt/100
+    y.append(np.arcsin(g)*2/np.pi*180)
+for i in x:
+    g=np.sin((i+1)/2/180*np.pi)
+    g=g*40*kopt/100
+    z.append(np.arcsin(g)*2/np.pi*180*1.5)
+print(x)
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei'] # 用来正常显示中文标签 微软雅黑-Microsoft YaHei,黑体-SimHei,仿宋-FangSong
+plt.rcParams['axes.unicode_minus'] = False # 用来正常显示负号
+p=[]
+p2=[]
+for i in range(180):
+    p.append(0.45*0.9*20*100*np.sin((x[i]+1)/2/180*np.pi)*np.sin(y[i]/2/180*np.pi)*228.549/0.45/0.9/20/100)
+    p2.append(0.45*0.9*20*100*np.sin((x[i]+1)/2/180*np.pi)*np.sin(z[i]/2/180*np.pi)*228.549/0.45/0.9/20/100)
+print(y)
+plt.title("偏移条件下的传输功率")
+plt.plot(x,y,label='原边脉宽角')
+#plt.plot(x,z,label='改进后原边脉宽角')
+plt.plot(x,p,label='最大传输功率')
+plt.xlabel('beta/°')
+#plt.plot(x,p2,label='改进后最大传输功率')
+plt.legend()
+plt.show()
